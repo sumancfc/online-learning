e6c36b26ddec44d2ee3a888a446a17c911284a18
Suman Shrestha <sumanstha999@gmail.com>
2022-12-04 21:00:18 +0100

    register page added on client side

---
+++ client/pages/register.js
@@ -0,0 +1,13 @@
+import Footer from "../components/Layout/Footer";
+import SignUp from "../components/SignUp.js";
+
+const Register = () => {
+  return (
+    <>
+      <SignUp />
+      <Footer />
+    </>
+  );
+};
+
+export default Register;
